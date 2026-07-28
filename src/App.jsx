3690656241d040af86cd6bdd2633187da964515f
eyezzzz <gabrielboalento3004@gmail.com>
2026-07-27 23:20:10 -0300

    Ativar modo de desempenho global

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { ModuleProvider } from './context/ModuleContext'
+import { PerformanceProvider } from './context/PerformanceContext'
 import { MODULES } from './config/modules'
 import { AppRouter } from './router/AppRouter'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <ModuleProvider modules={MODULES}>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
-      </ModuleProvider>
+      <PerformanceProvider>
+        <ModuleProvider modules={MODULES}>
+          <AuthProvider>
+            <AppRouter />
+          </AuthProvider>
+        </ModuleProvider>
+      </PerformanceProvider>
     </BrowserRouter>
   )
 }
