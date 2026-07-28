e8027ac48854a7bcddc1e714fc36db07690fa707
eyezzzz <gabrielboalento3004@gmail.com>
2026-07-28 10:43:26 -0300

    perf(agenda): remover observer global do bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './performance.css'
-import './agendaOperationalFixes'
 import App from './App'
 import { SiteLegalFooterPortal } from './public/components/SiteLegalFooterPortal'
 
