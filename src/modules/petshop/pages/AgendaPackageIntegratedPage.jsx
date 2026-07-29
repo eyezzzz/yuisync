47576b5321cfb0ac0e1a8067b884f8700a340ed5
eyezzzz <gabrielboalento3004@gmail.com>
2026-07-29 11:18:16 -0300

    fix(agenda): evitar duplicidade dos controles legados

--- src/modules/petshop/pages/AgendaPackageIntegratedPage.jsx
+++ src/modules/petshop/pages/AgendaPackageIntegratedPage.jsx
@@ -2,9 +2,17 @@ import AgendaIntegratedPage from './AgendaIntegratedPage'
 import AgendaBookingEnhancements from './AgendaBookingEnhancements'
 import AgendaNativePackageControls from './AgendaNativePackageControls'
 
+const NATIVE_PACKAGE_STYLES = `
+  [data-yuisync-package-summary-root],
+  [data-yuisync-package-picker-root] {
+    display: none !important;
+  }
+`
+
 export default function AgendaPackageIntegratedPage() {
   return (
     <>
+      <style>{NATIVE_PACKAGE_STYLES}</style>
       <AgendaIntegratedPage />
       <AgendaBookingEnhancements />
       <AgendaNativePackageControls />
