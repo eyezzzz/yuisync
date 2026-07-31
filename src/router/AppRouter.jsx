b0a37c377866ce763ae8841a0b4d45d6b679571f
eyezzzz <gabrielboalento3004@gmail.com>
2026-07-31 17:58:28 -0300

    fix(dashboard): montar resolvedor de nomes da agenda

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -15,6 +15,7 @@ import { PerformanceModeButton } from '../components/PerformanceModeButton'
 import { PetshopOperationsEnhancer } from '../modules/petshop/components/PetshopOperationsEnhancer'
 import { AgendaCardLayoutEnhancer } from '../modules/petshop/components/AgendaCardLayoutEnhancer'
 import { PackageRecurringScheduleEnhancer } from '../modules/petshop/components/PackageRecurringScheduleEnhancer'
+import { DashboardAgendaLabelsEnhancer } from '../modules/petshop/components/DashboardAgendaLabelsEnhancer'
 
 const LoginPage = lazy(() => import('../shared/pages/LoginPage'))
 const LauncherPage = lazy(() => import('../shared/pages/LauncherPage'))
@@ -181,6 +182,7 @@ function AppLayout() {
           <PetshopOperationsEnhancer />
           <AgendaCardLayoutEnhancer />
           <PackageRecurringScheduleEnhancer />
+          <DashboardAgendaLabelsEnhancer />
         </>
       )}
       {activeModuleId === 'system' && <SystemSupportPriorityAlert />}
